83e04233d4af0d465d1145e02136a85cdeffbfa2
Ben Elliston <bje@air.net.au>
2019-04-08 22:49:22 +1000

    * tets/test_utils.py: Use 'with' for automatic file closure.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,5 +4,6 @@ from cemo.utils import printstats
 def test_printstats(solution, capfd):
     printstats(solution)
     captured = capfd.readouterr()
-    f = open('tests/stats.txt', 'r')
-    assert captured.out == f.read()
+    with open('tests/stats.txt', 'r') as f:
+        array = f.read()
+    assert captured.out == array
