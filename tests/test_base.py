24508e60b5afef306c666ce8b84021ba9bd6c8f9
José Zapata <jotaigna@gmail.com>
2019-04-09 11:28:29 +1000

    tests/test_base.py Added docstrings

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,12 +1,14 @@
-# openCEM tests for basic instantiation
+"""openCEM tests for basic instantiation"""
 
 
 def test_model_creation(model, benchmark):
+    """Assert model is constructed matches benchmark"""
     assert model.name == benchmark.name, "Test model name does not match saved data"
-    assert model.is_constructed() is False
+    assert model.is_constructed() is False, "Model is constructed"
 
 
 def test_model_instantiation(instance, benchmark):
+    """Assert that instance is constructed from model and matches benchmark"""
     assert instance.name == benchmark.name, "Instance name does not match"
     assert instance.is_constructed() is True
     assert instance.zones.data() == benchmark.zones.data(), "Mismatch in test regions"
