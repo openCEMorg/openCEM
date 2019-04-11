53da2a800404b319cfd751a359e970b2cce1e721
José Zapata <jotaigna@gmail.com>
2019-04-11 15:14:09 +1000

    test/test_rules.py Added docstrings

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -1,3 +1,5 @@
+'''Test suite to check model rules'''
+
 import pytest
 from pyomo.environ import value
 
@@ -11,6 +13,7 @@ from cemo.rules import con_caplim, con_maxcap, con_opcap, dispatch
     (16, 12),
 ])
 def test_con_maxcap(solution, zone, tech):
+    '''Assert solution obeys maximum capacity constraints'''
     assert pytest.approx(value(con_maxcap(solution, zone, tech)))
 
 
@@ -25,6 +28,7 @@ def test_con_maxcap(solution, zone, tech):
     '2020-01-01 21:00:00',
 ])
 def test_con_caplim(solution, zone, tech, time):
+    '''Assert solution obeys capacity factor limitation constraint'''
     assert pytest.approx(value(con_caplim(solution, zone, tech, time)))
 
 
@@ -36,9 +40,11 @@ def test_con_caplim(solution, zone, tech, time):
     (16, 12)
 ])
 def test_con_opcap(solution, zone, tech):
+    '''Assert solution computes same operating capacity as benchmark'''
     assert pytest.approx(value(con_opcap(solution, zone, tech)))
 
 
 @pytest.mark.parametrize("region", [4, 5])
 def test_dispatch(solution, region):
+    '''Assert solution dispatch matches benchmark'''
     assert pytest.approx(value(dispatch(solution, region)))
