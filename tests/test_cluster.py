7c2d340bf8a92a81823ac49eb82860c2953e3d9b
José Zapata <jotaigna@gmail.com>
2019-04-17 11:07:28 +1000

    tests/test_cluster.py remove more pylint warnings

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -1,4 +1,5 @@
 '''Test suite for cluster module'''
+# pylint: disable=protected-access
 from difflib import SequenceMatcher
 
 import pytest
@@ -18,47 +19,48 @@ def test_cluster_instantiation():
 ])
 def test_cluster_size(cluster_no):
     '''Assert cluster sizes on request'''
-    a = cemo.cluster.CSVCluster()
-    a.clusterset(cluster_no)
-    assert a.Xcluster.size == cluster_no * 3
+    test_cluster = cemo.cluster.CSVCluster()
+    test_cluster.clusterset(cluster_no)
+    assert test_cluster.Xcluster.size == cluster_no * 3
 
 
 def test_cluster_data_files(model_options):
     '''Assert generated scenario files match a known good value'''
     clus = cemo.cluster.CSVCluster(max_d=6)
-    a = cemo.cluster.ClusterRun(clus, 'tests/CNEM.template', model_options)
-    a._gen_dat_files()
-    with open(a.tmpdir + '/S5.dat') as ff:
-        fromfile = ff.readlines()
-    with open('tests/CNEM5test.dat') as tf:
-        tofile = tf.readlines()
+    test_cluster = cemo.cluster.ClusterRun(clus, 'tests/CNEM.template', model_options)
+    test_cluster._gen_dat_files()
+    with open(test_cluster.tmpdir + '/S5.dat') as source:
+        fromfile = source.readlines()
+    with open('tests/CNEM5test.dat') as test:
+        tofile = test.readlines()
     # Check that they are mostly the same, but for gen_cap_initial tmpdir line
-    d = SequenceMatcher(None, fromfile, tofile)
-    assert d.ratio() >= 0.994
+    sequence = SequenceMatcher(None, fromfile, tofile)
+    assert sequence.ratio() >= 0.994
 
 
-def test_cluster_gen_scenario_struct(model_options):
+def test_cluster_scenario_structure(model_options):
     '''Assert generated scenario tree structure matches a good known value'''
     clus = cemo.cluster.CSVCluster(max_d=6)
-    a = cemo.cluster.ClusterRun(clus, 'tests/CNEM.template', model_options)
-    a._gen_scen_struct()
-    with open(a.tmpdir + '/ScenarioStructure.dat') as ff:
-        fromfile = ff.readlines()
-    with open('tests/ScenTest.dat') as tf:
-        tofile = tf.readlines()
+    test_cluster = cemo.cluster.ClusterRun(clus, 'tests/CNEM.template', model_options)
+    test_cluster._gen_scen_struct()
+    with open(test_cluster.tmpdir + '/ScenarioStructure.dat') as source:
+        fromfile = source.readlines()
+    with open('tests/ScenTest.dat') as test:
+        tofile = test.readlines()
     # Check that they are mostly the same
-    d = SequenceMatcher(None, fromfile, tofile)
-    assert d.ratio() >= 0.99999999
+    sequence = SequenceMatcher(None, fromfile, tofile)
+    assert sequence.ratio() >= 0.99999999
 
 
-def test_cluster_gen_ref_model(temp_data_dir, model_options):
+def test_cluster_gen_ref_model(model_options):
+    '''Assert reference model is being generated as a good known value'''
     clus = cemo.cluster.CSVCluster()
-    a = cemo.cluster.ClusterRun(clus, 'tests/CNEM.template', model_options)
-    a._gen_ref_model()
-    with open(a.tmpdir + '/ReferenceModel.py') as ff:
-        fromfile = ff.readlines()
-    with open('tests/ReferenceModel.py') as tf:
-        tofile = tf.readlines()
+    test_cluster = cemo.cluster.ClusterRun(clus, 'tests/CNEM.template', model_options)
+    test_cluster._gen_ref_model()
+    with open(test_cluster.tmpdir + '/ReferenceModel.py') as source:
+        fromfile = source.readlines()
+    with open('tests/ReferenceModel.py') as test:
+        tofile = test.readlines()
     # Check that they are mostly the same
-    d = SequenceMatcher(None, fromfile, tofile)
-    assert d.ratio() >= 0.999999999999
+    sequence = SequenceMatcher(None, fromfile, tofile)
+    assert sequence.ratio() >= 0.999999999999
