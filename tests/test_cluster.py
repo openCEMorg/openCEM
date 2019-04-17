d03dd7bc7139cd15a076ecebdf2ac9ebea11233c
José Zapata <jotaigna@gmail.com>
2019-04-17 14:09:39 +1000

    tests/test_cluster.py added next day and prev day unit tests

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -2,9 +2,11 @@
 # pylint: disable=protected-access
 from difflib import SequenceMatcher
 
+import datetime
 import pytest
 
 import cemo.cluster
+from cemo.cluster import next_weekday, prev_weekday
 
 
 def test_cluster_instantiation():
@@ -27,7 +29,8 @@ def test_cluster_size(cluster_no):
 def test_cluster_data_files(model_options):
     '''Assert generated scenario files match a known good value'''
     clus = cemo.cluster.CSVCluster(max_d=6)
-    test_cluster = cemo.cluster.ClusterRun(clus, 'tests/CNEM.template', model_options)
+    test_cluster = cemo.cluster.ClusterRun(
+        clus, 'tests/CNEM.template', model_options)
     test_cluster._gen_dat_files()
     with open(test_cluster.tmpdir + '/S5.dat') as source:
         fromfile = source.readlines()
@@ -35,13 +38,14 @@ def test_cluster_data_files(model_options):
         tofile = test.readlines()
     # Check that they are mostly the same, but for gen_cap_initial tmpdir line
     sequence = SequenceMatcher(None, fromfile, tofile)
-    assert sequence.ratio() >= 0.994
+    assert sequence.ratio() >= 1
 
 
 def test_cluster_scenario_structure(model_options):
     '''Assert generated scenario tree structure matches a good known value'''
     clus = cemo.cluster.CSVCluster(max_d=6)
-    test_cluster = cemo.cluster.ClusterRun(clus, 'tests/CNEM.template', model_options)
+    test_cluster = cemo.cluster.ClusterRun(
+        clus, 'tests/CNEM.template', model_options)
     test_cluster._gen_scen_struct()
     with open(test_cluster.tmpdir + '/ScenarioStructure.dat') as source:
         fromfile = source.readlines()
@@ -49,13 +53,14 @@ def test_cluster_scenario_structure(model_options):
         tofile = test.readlines()
     # Check that they are mostly the same
     sequence = SequenceMatcher(None, fromfile, tofile)
-    assert sequence.ratio() >= 0.99999999
+    assert sequence.ratio() >= 1
 
 
 def test_cluster_gen_ref_model(model_options):
     '''Assert reference model is being generated as a good known value'''
     clus = cemo.cluster.CSVCluster()
-    test_cluster = cemo.cluster.ClusterRun(clus, 'tests/CNEM.template', model_options)
+    test_cluster = cemo.cluster.ClusterRun(
+        clus, 'tests/CNEM.template', model_options)
     test_cluster._gen_ref_model()
     with open(test_cluster.tmpdir + '/ReferenceModel.py') as source:
         fromfile = source.readlines()
@@ -63,4 +68,16 @@ def test_cluster_gen_ref_model(model_options):
         tofile = test.readlines()
     # Check that they are mostly the same
     sequence = SequenceMatcher(None, fromfile, tofile)
-    assert sequence.ratio() >= 0.999999999999
+    assert sequence.ratio() >= 1
+
+
+def test_cluster_next_weekday():
+    '''assert next_weekday works as intended'''
+    assert next_weekday(datetime.date(2019, 4, 2), 2) == datetime.date(2019, 4, 3)
+    assert next_weekday(datetime.date(2019, 4, 2), 0) == datetime.date(2019, 4, 8)
+
+
+def test_cluster_prev_weekday():
+    '''assert prev_weekday works as intended'''
+    assert prev_weekday(datetime.date(2019, 4, 2), 2) == datetime.date(2019, 3, 27)
+    assert prev_weekday(datetime.date(2019, 4, 2), 0) == datetime.date(2019, 4, 1)
