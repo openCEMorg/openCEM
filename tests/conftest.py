aa4efaf61487a6cdc4db286ba3371c22e0355c88
José Zapata <jotaigna@gmail.com>
2019-04-17 15:21:07 +1000

    test/conftest.py added docstrings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-# Common fixtures for test suite
+'''Common fixtures for test suite'''
 import pickle
 
 import pytest
@@ -12,6 +12,7 @@ from cemo.model import create_model
                     'CTV_trans',
                 ])
 def model(request):
+    '''Model fixture for tests, returns an openCEM model'''
     return create_model(request.param,
                         unslim=True,
                         emitlimit=True,
@@ -24,6 +25,7 @@ def model(request):
 
 @pytest.fixture(scope="session")
 def model_options():
+    '''Model options fixture for tests'''
     options = {}
     options.update({'emitlimit': True})
     options.update({'nem_ret_gwh': False})
@@ -36,17 +38,21 @@ def model_options():
 
 @pytest.fixture(scope="session")
 def instance(model):
+    '''Model instance fixture based on model fixture'''
     data = 'tests/' + model.name + '.dat'
     return model.create_instance(data)
 
 
 @pytest.fixture
 def benchmark(instance):
+    '''Benchmark fixture for tests is a solved model instance
+    stored as a pickled form'''
     pickledDataPath = 'tests/' + instance.name + '.p'
     return pickle.load(open(pickledDataPath, "rb"))
 
 
 def pytest_addoption(parser):
+    '''Solver options for solution fixture'''
     parser.addoption(
         "--solver",
         action="store",
@@ -57,6 +63,7 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="session")
 def solution(request, instance):
+    '''Solution fixture is an instance solved on the spot using local solver'''
     solver = request.config.getoption("--solver")
     sol = SolverFactory(solver)
     assert sol.solve(instance)
@@ -65,4 +72,5 @@ def solution(request, instance):
 
 @pytest.fixture(scope="session")
 def temp_data_dir(tmpdir_factory):
+    '''Temporary data directory fixture for tests'''
     return tmpdir_factory.mktemp("CEMOtemp")
