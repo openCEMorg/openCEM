4b46b58c8462a34b54c8c8c33d3f3a9f749aa3de
José Zapata <jotaigna@gmail.com>
2019-04-17 11:21:23 +1000

    tests/test_utils.py Removed pylint warnings

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,11 @@
+'''Test suite for utils module'''
 from cemo.utils import printstats
 
 
 def test_printstats(solution, capfd):
+    '''Assert that solution stats printout matches known value'''
     printstats(solution)
     captured = capfd.readouterr()
-    with open('tests/stats.txt', 'r') as f:
-        array = f.read()
+    with open('tests/stats.txt', 'r') as sample:
+        array = sample.read()
     assert captured.out == array
