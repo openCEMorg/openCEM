407c18997c53315a4bf88868eac99bc926b1af4e
José Ignacio Zapata Fuentealba <jose.zapata@itpau.com.au>
2019-09-10 11:09:01 +1000

    Update units and results that test for strategic reserve being generated for simulations

--- tests/ReferenceModel.py
+++ tests/ReferenceModel.py
@@ -6,4 +6,5 @@ model = create_model('openCEM',
                      nem_ret_ratio=False,
                      nem_ret_gwh=False,
                      region_ret_ratio=False,
-                     nem_disp_ratio=False)
+                     nem_disp_ratio=False,
+                     no_disp_strategic_reserve=True)
