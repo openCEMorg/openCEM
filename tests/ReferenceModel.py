c51419f3d9bc602af83cc3b61e988f5d2da8f6ce
José Ignacio Zapata Fuentealba <jose.zapata@itpau.com.au>
2019-09-11 12:16:12 +1000

    Updating test suite results

--- tests/ReferenceModel.py
+++ tests/ReferenceModel.py
@@ -7,4 +7,4 @@ model = create_model('openCEM',
                      nem_ret_gwh=False,
                      region_ret_ratio=False,
                      nem_disp_ratio=False,
-                     no_disp_strategic_reserve=True)
+                     disp_strategic_reserve=False)
