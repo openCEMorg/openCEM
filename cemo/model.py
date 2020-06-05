dc8eedb14eee9ee5a351b3dde85d6695e9adc687
Jose Zapata <jzapata@itpau.com.au>
2020-06-05 15:08:11 +1000

    improves reliability performance of dispatch simulations, as now they seem to consistently achieve sufficient capacity

--- cemo/model.py
+++ cemo/model.py
@@ -45,7 +45,7 @@ from cemo.rules import (ScanForHybridperZone, ScanForStorageperZone,
 
 def model_options(**kwargs):
     '''Container for model options'''
-    FIELDS = {'unslim': False,
+    FIELDS = {'unslim': True,
               'nem_emit_limit': False,
               'nem_ret_ratio': False,
               'nem_ret_gwh': False,
